3c11f208f4928bb0e2cae9bc0f88f2f5d383b2a1
vip-cod-er <65621418+vip-cod-er@users.noreply.github.com>
2020-05-20 00:18:46 +0530

    Create program1.py

---
+++ Day1/program1.py
@@ -0,0 +1,10 @@
+name = input("Enter your name:") #asking name
+branch = input("What is your Branch:")#asking for the branch
+gender= input("Write your gender:")#asking for gender
+college=input("From which college you belong to?")#asking for college
+age=input("Write your age:")#asking the age
+print("My name is : ",name)
+print("My branch is :",branch)
+print("I am",gender)
+print("I am from",college)
+print("I am of ",age)
